746bb3368c52de1a784402f7af3a99141abc2464
jraynaldi3 <jraynaldi3@gmail.com>
2023-08-06 14:33:38 +0700

    fixing the scripts

--- certora/conf/gambitToDistributor.sh
+++ certora/conf/gambitToDistributor.sh
@@ -1,4 +1,4 @@
 gambit mutate --filename contracts/rewards/RewardsDistributor.sol \
     --solc_remappings @aave=node_modules/@aave \
     --num_mutants 20 \
-    --mutations assignment-mutation \
+    --mutations delete-expression-mutation\
